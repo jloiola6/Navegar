b5032f98e939a8125adf0ce91fbc7ee056669efb
joaon <joaon.cavok@gmail.com>
2024-01-02 22:54:39 -0300

    Resolvendo leitura de arquivos estaticos

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('apps.user.urls')),
     path('', include('apps.core.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
