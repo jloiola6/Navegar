e201fbc02dbd6a66489372d11a75468754309ef3
joaon <joaon.cavok@gmail.com>
2024-01-02 23:06:26 -0300

    Lendo arquivos estativos em DEBUG

--- website/website/settings.py
+++ website/website/settings.py
@@ -135,6 +135,10 @@ STATIC_URL = 'front/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'front/static')
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'front/static')
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
