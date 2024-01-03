42b835f2cc416105a44c88b9db6e1820e331ab2d
Érick Fernandes <erickfernandess2@gmail.com>
2024-01-02 20:56:08 -0500

    Iniciando estilos globais

--- website/website/settings.py
+++ website/website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_q9k1$-qv=s1hr0=c1pmvmneh&y=_dcfw(occz7g!x3ychw=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
