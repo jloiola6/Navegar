3be58c4f52444efa9d0b5dc9accaf87b169339e2
João Teixeira <jloiola6@outlook.com>
2024-02-20 23:12:22 -0300

    Adicionar novo calculo para valores da rota Fixes #18

--- website/apps/route/forms.py
+++ website/apps/route/forms.py
@@ -31,7 +31,14 @@ RouteWeekdayFormSet = inlineformset_factory(Route, RouteWeekday, form=RouteWeekd
 class RouteForm(forms.ModelForm):
     departure_time = forms.TimeField(widget=TextInput(attrs={'type': 'time'}))
     arrival_time = forms.TimeField(widget=TextInput(attrs={'type': 'time'}))
+    
+    #Adicionar campos readonly aqui!
+    # value_3 = forms.TimeField(widget=TextInput(attrs={'readonly': True}))
+
+    def __init__(self, *args, **kwargs):
+        super(RouteForm, self).__init__(*args, **kwargs)
+        
 
     class Meta:
         model = Route
-        fields = ['origin', 'destination', 'value', 'departure_time', 'arrival_time', 'after_midnight']
+        fields = ['origin', 'destination', 'value', 'value_2', 'value_3', 'value_4', 'departure_time', 'arrival_time', 'after_midnight']
