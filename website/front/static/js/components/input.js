f819409527be1db69c78bdecb589b5dc6f8102be
Érick Fernandes <erickfernandess2@gmail.com>
2024-03-03 16:45:42 -0500

    Estilizar partial de visualização de ticket Fixes #51

--- website/front/static/js/components/input.js
+++ website/front/static/js/components/input.js
@@ -1,9 +1,14 @@
 const inputs = document.querySelectorAll(':is(input, select)')
 
+const toogleTouchedClass = (event) => {
+    event.target.classList.add('touched')
+}
+
 const initialize = (input) => {
-    input.addEventListener('blur', () => {
-        input.classList.add('touched')
-    })
+    input.addEventListener('blur', toogleTouchedClass)
+
+    if(input.type == 'file')
+        input.addEventListener('change', toogleTouchedClass)
 }
 
 if(inputs){
