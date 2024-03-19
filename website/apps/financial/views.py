29d1a87833ffe2d23bd0ab9f2625636e6ab256f2
Érick Fernandes <erickfernandess2@gmail.com>
2024-03-18 19:31:28 -0500

    Concluindo versão inicial da pa´gina de finanças

--- website/apps/financial/views.py
+++ website/apps/financial/views.py
@@ -1,6 +1,7 @@
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from django.db.models import Sum
 
 from apps.ticket.models import Ticket
 
@@ -12,5 +13,10 @@ def index(request):
     if request.method == 'POST':
         searched_date = request.POST.get('date')
         tickets = Ticket.objects.filter(created_at__date= searched_date)
+
+    total_cost = tickets.aggregate(Sum('cost'))['cost__sum']
+    total_value = tickets.aggregate(Sum('value'))['value__sum']
+
+    total_profit = total_value - total_cost
 
     return TemplateResponse(request, 'financial/index.html', locals())
