b325496543c86fe4e81aaa36b2d5beab27cbac5a
Érick Fernandes <erickfernandess2@gmail.com>
2024-03-19 22:01:11 -0500

    Impedir de cadastrar o mesmo local duas vezes Fixes #110

--- website/apps/route/models.py
+++ website/apps/route/models.py
@@ -27,7 +27,7 @@ class Boat(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique= True)
 
     def __str__(self):
         return self.name
